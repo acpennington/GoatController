63209602ff73e5b9cc1a0e02ef8502b028528a45
Allen Pennington <acpenni@evoforge.com>
2021-02-24 16:31:08 -0500

    field redux setup

--- web-app/src/stateStore/reducers/index.js
+++ web-app/src/stateStore/reducers/index.js
@@ -4,10 +4,12 @@ import { connectRouter } from "connected-react-router";
 
 import hoverCard from "./hoverCard.js";
 import selectedCard from "./selectedCard.js";
+import field from "./field.js"
 
 export default (history) =>
    combineReducers({
       router: connectRouter(history),
+      field,
       hoverCard,
       selectedCard
    });
