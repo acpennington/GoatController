e5e220538ef0286ac02d0495a9778baf997e02cb
Allen Pennington <acpenni@evoforge.com>
2021-03-12 15:46:40 -0500

    removed shuffle function

--- web-app/src/stateStore/reducers/field.js
+++ web-app/src/stateStore/reducers/field.js
@@ -17,7 +17,7 @@ const initialState = {
       sleeves: "exarion.png",
       lifepoints: 8000,
       deck: { count: 35 },
-      graveyard: [],
+      graveyard: [{ name: "Call of the Haunted" }],
       banished: [],
       hand: [
          { name: FACEDOWN_CARD },
@@ -107,24 +107,4 @@ export default function (state = initialState, action) {
       default:
          return state;
    }
-}
-
-function shuffle(array) {
-   var currentIndex = array.length,
-      temporaryValue,
-      randomIndex;
-
-   // While there remain elements to shuffle...
-   while (0 !== currentIndex) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-   }
-
-   return array;
 }
