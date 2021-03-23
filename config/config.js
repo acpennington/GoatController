306702e5bee03dcb8fe32bce156e8b5fa0d88be9
Allen C Pennington <penningtona@mail.usf.edu>
2021-03-23 19:56:18 -0400

    aws config

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+   aws_table_name: "users",
+   aws_remote_config: {
+      accessKeyId: "AKIAYJS555CFSKED66VX",
+      secretAccessKey: "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",
+      region: "us-east-2"
+   }
+};
