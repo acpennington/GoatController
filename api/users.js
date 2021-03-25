2fdd9146aa673144196200350b25bdebe46e9902
Allen C Pennington <penningtona@mail.usf.edu>
2021-03-24 20:33:31 -0400

    got a route goin

--- api/users.js
+++ api/users.js
@@ -17,7 +17,7 @@ router.post(
       check("email", "Please include a valid email").isEmail(),
       check("password", "Please use a password with 10 or more characters").isLength({ min: 10 })
    ],
-   (req, res) => {
+   async (req, res) => {
       const errors = validationResult(req);
       if (errors.isEmpty()) {
          const { name, email, password } = req.body;
@@ -30,17 +30,17 @@ router.post(
             }
          };
 
-         DynamoDB.getItem(params, (err, data) => {
+         await DynamoDB.getItem(params, (err, data) => {
             console.log("hello");
             if (err) console.log(err);
             else console.log(data.Item);
-         });
+         }).promise();
 
          res.send("done");
 
          // Encrypt password
 
-         // Return response
+         // Return a JWT (so they can set an email/phone number for password recovery)
       } else res.status(400).json({ errors: errors.array() });
    }
 );
