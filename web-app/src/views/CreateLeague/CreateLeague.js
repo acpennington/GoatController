fd99e71133f0f799baf48271799e7d50f7cfb593
Allen Pennington <acpenni@evoforge.com>
2021-06-04 11:51:20 -0400

    gave createleague page the template

--- web-app/src/views/CreateLeague/CreateLeague.js
+++ web-app/src/views/CreateLeague/CreateLeague.js
@@ -1,1 +1,9 @@
-// placeholder for createleague page
+import React, { PureComponent } from "react";
+
+import PageTemplate from "components/Header/PageTemplate.js";
+
+class CreateLeague extends PureComponent {
+    render() {
+        <PageTemplate></PageTemplate>
+    }
+}
