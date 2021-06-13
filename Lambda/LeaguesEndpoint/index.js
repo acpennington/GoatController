8853068d91db146fe8de255650ff92b692588788
acpennington <penningtona@mail.usf.edu>
2021-06-13 11:18:58 -0400

    added files to Leagues Endpoint

--- Lambda/LeaguesEndpoint/index.js
+++ Lambda/LeaguesEndpoint/index.js
@@ -1,4 +1,13 @@
-// @route POST api/leagues
-// @desc Create a new league
-// @access Private
-// @db 1 read, 1 write
+const post = require("./post.js");
+
+// Routes API requests to the appropriate function
+exports.handler = async (event) => {
+   const { body, token, httpmethod, path } = event;
+
+   switch (httpmethod) {
+      case "POST":
+         return await post(body, token);
+      default:
+         return { statusCode: 400, body: { errors: [{ msg: "Invalid HTTP method" }] } };
+   }
+};
