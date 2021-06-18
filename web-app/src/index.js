a2b16c002612ae58dbe80609ad1a442e7f493b63
acpennington <penningtona@mail.usf.edu>
2021-06-18 17:23:31 -0400

    deleted some pages

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -6,11 +6,9 @@ import { history } from "./stateStore/gameStore.js";
 import "assets/scss/material-kit-react.scss?v=1.9.0";
 
 // pages for this product
-import Components from "views/Components/Components.js";
 import CardFinderDemo from "views/Components/CardFinderDemo.js";
 import TextToCardDemo from "views/Components/TextToCardDemo.js";
 import SettingsPage from "views/SettingsPage/SettingsPage.js";
-import LandingPage from "views/LandingPage/LandingPage.js";
 import LoginPage from "views/LoginPage/LoginPage.js";
 import Game from "views/Game/Game.js";
 import Wall from "views/Wall/Wall.js";
@@ -26,11 +24,10 @@ ReactDOM.render(
          <Route path="/createleague" component={CreateLeague} />
          <Route path="/game" component={Game} />
          <Route path="/solo" render={() => <Game solo={true} />} />
-         <Route path="/landing-page" component={LandingPage} />
          <Route path="/settings" component={SettingsPage} />
          <Route path="/login-page" component={LoginPage} />
          <Route path="/wall" component={Wall} />
-         <Route path="/" component={Components} />
+         <Route path="/" component={LoginPage} />
       </Switch>
    </Router>,
    document.getElementById("root")
--- web-app/src/views/Components/CardFinderDemo.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import CardFinder from "components/CardFinder/CardFinder.js"
-
-export default function CardFinderDemo() {
-    return <div style={{backgroundColor: "white", width: "100vw", height: "100vh"}}><CardFinder withFusions /></div>
-}
