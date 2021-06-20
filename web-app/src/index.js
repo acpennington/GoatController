7dc22c37390871dc2dc6c5dace2cd067754d9ddd
acpennington <penningtona@mail.usf.edu>
2021-06-19 21:47:09 -0400

    added route for league page

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -11,11 +11,13 @@ import LoginPage from "views/LoginPage/LoginPage.js";
 import Game from "views/Game/Game.js";
 import Wall from "views/Wall/Wall.js";
 import Leagues from "views/Leagues/Leagues.js";
+import LeaguePage from "views/LeaguePage/LeaguePage.js";
 import CreateLeague from "views/CreateLeague/CreateLeague.js";
 
 ReactDOM.render(
    <Router history={history}>
       <Switch>
+         <Route path="/league" component={LeaguePage} />
          <Route path="/leagues" component={Leagues} />
          <Route path="/createleague" component={CreateLeague} />
          <Route path="/game" component={Game} />
