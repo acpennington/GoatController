135e9a6be27bc23a726bd4787a3f6e23d39910c0
acpennington <penningtona@mail.usf.edu>
2021-06-27 10:46:20 -0400

    added protocol check

--- Lambda/LeagueSocket/OnConnect/index.js
+++ Lambda/LeagueSocket/OnConnect/index.js
@@ -1,5 +1,7 @@
-exports.handler = async (event) => {
-   // we should use protocol and subprotocol for onconnect function
+// we should use protocol and subprotocol for onconnect function
 
-   return { statusCode: 200, body: "Connected." };
+exports.handler = async (event) => {
+   const headers = event.headers;
+   if (!headers || !headers["sec-websocket-protocol"]) return { statusCode: 400 };
+   else return { statusCode: 200, body: "Connected." };
 };
