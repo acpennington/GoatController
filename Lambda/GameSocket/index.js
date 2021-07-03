e278432197049618acaf84be22a27b63e7258730
acpennington <penningtona@mail.usf.edu>
2021-07-03 19:38:02 -0400

    more stuff for game socket

--- Lambda/GameSocket/index.js
+++ Lambda/GameSocket/index.js
@@ -1,1 +1,19 @@
-// Routing for GameSocket
+const auth = require("./utils/middleware.js");
+// Routes GameSocket Actions
+
+exports.handler = async (event) => {
+   const { requestContext } = event;
+   const body = JSON.parse(event.body);
+   const { action, data } = body;
+   const { id, token } = data;
+
+   const username = auth(token);
+   if (!username) return { statusCode: 401, body: { errors: [{ msg: "Unauthorized, token invalid" }] } };
+
+   switch (action) {
+      case "JoinMatch":
+         return;
+      default:
+         return { statusCode: 400, body: { errors: [{ msg: "Invalid action name" }] } };
+   }
+};
---
+++ Lambda/GameSocket/utils/sendChatMessage.js
@@ -0,0 +1,1 @@
+// This will be a function that sends a chat message to users connected to the game
