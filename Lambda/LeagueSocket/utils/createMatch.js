8ffeae848cf77fb92c1c69fedc8453bbc072ef29
acpennington <penningtona@mail.usf.edu>
2021-07-04 21:06:52 -0400

    createMatch needs to be tested to ensure that gamestate initialization works properly

--- Lambda/LeagueSocket/utils/createMatch.js
+++ Lambda/LeagueSocket/utils/createMatch.js
@@ -6,7 +6,7 @@ const { blankField } = require("./config/config.js");
 
 // @function createMatch
 // @desc Places two players in a match together
-// @db 2 reads, 3 writes
+// @db 1 reads, 3 writes
 async function createMatch(leagueId, playersParam, api) {
    const [player1, player2] = playersParam;
    const { name: player1name, connectionId: player1connection } = player1;
@@ -20,11 +20,24 @@ async function createMatch(leagueId, playersParam, api) {
 
    const goingFirstPlayer = Math.random() > 0.5 ? player1name : player2name;
 
-   const gamestate = {};
-   await setGamestate(gamestate, player1name, goingFirstPlayer);
-   await setGamestate(gamestate, player2name, goingFirstPlayer);
+   let params = {
+      RequestItems: {
+         users: {
+            Keys: [{ username: player1name }, { username: player2name }],
+            AttributesToGet: ["username", "decks", "activeDeck", "settings"]
+         }
+      }
+   };
+   const result = await DynamoDB.batchGet(params, (err) => {
+      if (err) return { statusCode: 400, body: { errors: [err] } };
+   }).promise();
+   const players = result.Items;
 
-   const params = {
+   const gamestate = {};
+   await setGamestate(gamestate, players[0], goingFirstPlayer);
+   await setGamestate(gamestate, players[1], goingFirstPlayer);
+
+   params = {
       TableName: "matches",
       Item: {
          id: matchId,
@@ -47,26 +60,18 @@ function stripSpecialChars(aString) {
    return aString.replace(/[^a-zA-Z0-9_]/g, "");
 }
 
-async function setGamestate(gamestate, playerName, goingFirstPlayer) {
-   gamestate[playerName] = JSON.parse(JSON.stringify(blankField));
+async function setGamestate(gamestate, player, goingFirstPlayer) {
+   const { username, decks, activeDeck, settings } = player;
+   gamestate[username] = JSON.parse(JSON.stringify(blankField));
 
-   const params = {
-      TableName: "users",
-      Key: { id: playerName }
-   };
-   const result = await DynamoDB.get(params, (err) => {
-      if (err) return { statusCode: 400, body: { errors: [err] } };
-   }).promise();
-   const player = result.Item;
-
-   const shuffledDeck = shuffle(player.decks[player.activeDeck].maindeck);
+   const shuffledDeck = shuffle(decks[activeDeck].maindeck);
    const hand = [];
-   const drawNumber = goingFirstPlayer === playerName ? 6 : 5;
+   const drawNumber = goingFirstPlayer === username ? 6 : 5;
    for (let i = 0; i < drawNumber; i++) hand.push(shuffledDeck.pop());
 
-   gamestate[playerName].sleeves = player.settings.sleeves;
-   gamestate[playerName].hand = hand;
-   gamestate[playerName].deck = shuffledDeck;
+   gamestate[username].sleeves = settings.sleeves;
+   gamestate[username].hand = hand;
+   gamestate[username].deck = shuffledDeck;
 }
 
 function shuffle(deck) {
