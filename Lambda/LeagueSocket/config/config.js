c7f865b33a818aa9f32532e493fdbca212b9e25b
acpennington <penningtona@mail.usf.edu>
2021-07-04 20:23:03 -0400

    updated createMatch needs testing

--- Lambda/LeagueSocket/config/config.js
+++ Lambda/LeagueSocket/config/config.js
@@ -1,3 +1,16 @@
 module.exports = {
-   jwtsecret: "DeezNuts69"
+   jwtsecret: "DeezNuts69",
+   blankField: {
+      sleeves: "Goat.png",
+      lifepoints: 8000,
+      deck: [],
+      graveyard: [],
+      banished: [],
+      usedFusions: {},
+      hand: [],
+      handRevealed: false,
+      "s/t": [null, null, null, null, null],
+      "field spell": null,
+      monster: [null, null, null, null, null]
+   }
 };
--- Lambda/LeagueSocket/utils/createMatch.js
+++ Lambda/LeagueSocket/utils/createMatch.js
@@ -2,9 +2,11 @@ const AWS = require("aws-sdk");
 AWS.config.update({ region: "us-east-2" });
 const DynamoDB = new AWS.DynamoDB.DocumentClient();
 
+const { blankField } = require("./config/config.js");
+
 // @function createMatch
 // @desc Places two players in a match together
-// @db 0 reads, 3 writes
+// @db 2 reads, 3 writes
 async function createMatch(leagueId, playersParam, api) {
    const [player1, player2] = playersParam;
    const { name: player1name, connectionId: player1connection } = player1;
@@ -16,24 +18,72 @@ async function createMatch(leagueId, playersParam, api) {
    players[player1name] = "";
    players[player2name] = "";
 
-   let params = {
+   const goingFirstPlayer = Math.random() > 0.5 ? player1name : player2name;
+
+   const gamestate = {};
+   await setGamestate(gamestate, player1name, goingFirstPlayer);
+   await setGamestate(gamestate, player2name, goingFirstPlayer);
+
+   const params = {
       TableName: "matches",
       Item: {
          id: matchId,
          league: leagueId,
          players,
          watchers: [],
-         gamestate: [],
-         chat: [{ author: "Server", content: "New match started. Good luck to both players." }]
+         gamestate,
+         chat: [{ author: "Server", content: "New match started. " + goingFirstPlayer + " will go first. Good luck to both players." }]
       }
    };
    await DynamoDB.put(params, (err) => {
       if (err) return { statusCode: 400, body: { errors: [err] } };
    }).promise();
 
-   params = {
+   await updatePlayer(player1name, matchId, api);
+   await updatePlayer(player2name, matchId, api);
+}
+
+function stripSpecialChars(aString) {
+   return aString.replace(/[^a-zA-Z0-9_]/g, "");
+}
+
+async function setGamestate(gamestate, playerName, goingFirstPlayer) {
+   gamestate[playerName] = JSON.parse(JSON.stringify(blankField));
+
+   const params = {
       TableName: "users",
-      Key: { username: player1name },
+      Key: { id: playerName }
+   };
+   const result = await DynamoDB.get(params, (err) => {
+      if (err) return { statusCode: 400, body: { errors: [err] } };
+   }).promise();
+   const player = result.Item;
+
+   const shuffledDeck = shuffle(player.decks[player.activeDeck].maindeck);
+   const hand = [];
+   const drawNumber = goingFirstPlayer === playerName ? 6 : 5;
+   for (let i = 0; i < drawNumber; i++) hand.push(shuffledDeck.pop());
+
+   gamestate[playerName].sleeves = player.settings.sleeves;
+   gamestate[playerName].hand = hand;
+   gamestate[playerName].deck = shuffledDeck;
+}
+
+function shuffle(deck) {
+   for (let i = deck.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const temp = deck[i];
+      deck[i] = deck[j];
+      deck[j] = temp;
+   }
+
+   return deck;
+}
+
+async function updatePlayer(playerName, matchId, api) {
+   const params = {
+      TableName: "users",
+      Key: { username: playerName },
       UpdateExpression: "SET activeMatch = :matchid",
       ExpressionAttributeValues: { ":matchid": matchId }
    };
@@ -41,18 +91,8 @@ async function createMatch(leagueId, playersParam, api) {
       if (err) return { statusCode: 400, body: { errors: [err] } };
    }).promise();
 
-   params.Key = { username: player2name };
-   await DynamoDB.update(params, (err) => {
-      if (err) return { statusCode: 400, body: { errors: [err] } };
-   }).promise();
-
    const payload = { action: "NewGame", data: matchId };
    await api.postToConnection({ ConnectionId: player1connection, Data: JSON.stringify(payload) }).promise();
-   await api.postToConnection({ ConnectionId: player2connection, Data: JSON.stringify(payload) }).promise();
-}
-
-function stripSpecialChars(aString) {
-   return aString.replace(/[^a-zA-Z0-9_]/g, "");
 }
 
 module.exports = createMatch;
