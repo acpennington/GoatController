5696c2b1b123655ef705119f90f72bae3b4a0c29
acpennington <penningtona@mail.usf.edu>
2021-07-12 19:03:01 -0400

    concession done on front end

---
+++ web-app/src/views/Game/Battlefield/RightTools/StandardTools/ConcedeButton.js
@@ -0,0 +1,44 @@
+import React, { PureComponent } from "react";
+import { connect } from "react-redux";
+
+import Button from "components/CustomButtons/Button.js";
+import { WebSocketContext } from "views/Game/WebSocketContext";
+import { PLAYER_CONCEDED, CLEANUP_GAME } from "utils/constants.js";
+
+class ConcedeButton extends PureComponent {
+   render() {
+      const { concessionLink } = this.props;
+      const color = concessionLink ? "primary" : "danger";
+      const func = concessionLink ? () => quitGame(this.context) : () => concedeGame(this.context);
+
+      return (
+         <Button color={color} fullWidth round href={concessionLink} onClick={func}>
+            {concessionLink ? "Quit" : "Concede"}
+         </Button>
+      );
+   }
+}
+
+function concedeGame(socket) {
+   if (socket && socket.api) {
+      const payload = { action: PLAYER_CONCEDED, data: { token: socket.token, id: socket.matchId } };
+      socket.api.send(JSON.stringify(payload));
+   }
+}
+
+function quitGame(socket) {
+   window.sessionStorage.removeItem("activeMatch");
+
+   if (socket && socket.api) {
+      const payload = { action: CLEANUP_GAME, data: { token: socket.token, id: socket.matchId } };
+      socket.api.send(JSON.stringify(payload));
+   }
+}
+
+function mapStateToProps(state) {
+   return { concessionLink: state.settings.concessionLink };
+}
+
+ConcedeButton.contextType = WebSocketContext;
+
+export default connect(mapStateToProps)(ConcedeButton);
