17b48cd96c6c24cb6413410a7e5cb55aa8b0ef88
Allen Pennington <acpenni@evoforge.com>
2021-07-14 13:54:14 -0400

    added async

--- web-app/src/views/LeaguePage/LeaguePage.js
+++ web-app/src/views/LeaguePage/LeaguePage.js
@@ -38,7 +38,7 @@ class LeaguePage extends PureComponent {
       };
    }
 
-   componentDidMount() {
+   async componentDidMount() {
       if (this.state.name === LOADING) await this.fetchLeague();
       else this.setState({ description: OFFICIAL_UNRANKED.description, allowExarion: true, autoApprove: true, allowMultis: true, useQueue: false });
    }
