b7c2f73a98f7f1e649216d214a55559293cfc1c9
acpennington <penningtona@mail.usf.edu>
2021-07-17 11:46:59 -0400

    saving space

--- web-app/src/components/YugiohCardExpanded/CardScript.js
+++ web-app/src/components/YugiohCardExpanded/CardScript.js
@@ -67,11 +67,8 @@ class CardScript extends PureComponent {
       }
 
       const verb = (number) => (number === 1 ? " was " : " were ");
-      addMessage(
-         "Game",
-         heroPlayer + " flipped a coin " + count + " times. " + heads + verb(heads) + " heads, and " + tails + verb(tails) + "tails.",
-         this.context
-      );
+      const message = heroPlayer + " flipped a coin " + count + " times. " + heads + verb(heads) + " heads, and " + tails + verb(tails) + "tails.";
+      addMessage("Game", message, this.context);
    };
 
    render() {
