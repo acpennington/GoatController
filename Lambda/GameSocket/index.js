ef8f910240565446d77c47256a1c42aa191e3a54
acpennington <penningtona@mail.usf.edu>
2021-07-16 22:57:46 -0400

    removed unnecessary lambda routes

--- Lambda/GameSocket/index.js
+++ Lambda/GameSocket/index.js
@@ -5,8 +5,6 @@ const auth = require("./utils/middleware.js");
 const joinMatch = require("./JoinMatch.js");
 const newChatMessage = require("./NewChatMessage.js");
 const newPhase = require("./NewPhase.js");
-const nextPhase = require("./NextPhase.js");
-const prevPhase = require("./PrevPhase.js");
 const sendLpChange = require("./SendLpChange.js");
 const sendTokens = require("./SendTokens.js");
 const sendReveal = require("./SendReveal.js");
@@ -42,10 +40,6 @@ exports.handler = async (event) => {
       case "NewPhase":
          return await newPhase(id, username, data.data, connectionId, api);
       case "PushNextPhase":
-         return await nextPhase(id, username, connectionId, api);
-      case "PushPrevPhase":
-         return await prevPhase(id, username, connectionId, api);
-      case "SendLpChange":
          return await sendLpChange(id, username, data.amount, data.currentLP, connectionId, api);
       case "SendTokens":
          return await sendTokens(id, username, data.params, connectionId, api);
