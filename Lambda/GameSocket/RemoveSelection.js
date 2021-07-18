5727923ce714eaecf3732e63cfb6a537d46a749b
acpennington <penningtona@mail.usf.edu>
2021-07-17 22:15:56 -0400

    targeting seems to work

---
+++ Lambda/GameSocket/RemoveSelection.js
@@ -0,0 +1,13 @@
+const actionAndMessage = require("./utils/actionAndMessage.js");
+
+// @action RemoveSelection
+// @desc Sends a chat message from one player to the other (and watchers)
+// @access Private
+// @db 1 read, 0 writes
+async function removeSelection(id, username, connectionId, api) {
+   const action = { action: "CLEAR_SELECTION", data: { player: username } };
+   await actionAndMessage(id, action, false, connectionId, api);
+   return { statusCode: 200, body: "Removed selection" };
+}
+
+module.exports = removeSelection;
