2b7114bf6b70ed4f2c302d91cee50af829fb065e
acpennington <penningtona@mail.usf.edu>
2021-07-23 23:36:34 -0400

    remove param

--- Lambda/GameSocket/RemoveSelection.js
+++ Lambda/GameSocket/RemoveSelection.js
@@ -4,7 +4,7 @@ const actionAndMessage = require("./utils/actionAndMessage.js");
 // @desc Sends a chat message from one player to the other (and watchers)
 // @access Private
 // @db 1 read, 0 writes
-async function removeSelection(id, username, connectionId, api) {
+async function removeSelection(id, connectionId, api) {
    const action = { action: "CLEAR_SELECTION" };
    await actionAndMessage(id, action, false, connectionId, api);
    return { statusCode: 200, body: "Removed selection" };
