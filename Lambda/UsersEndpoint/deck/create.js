effbb2582284484faf055cb45961f74110e756cd
acpennington <penningtona@mail.usf.edu>
2021-07-24 22:14:24 -0400

    add dynamo import

--- Lambda/UsersEndpoint/deck/create.js
+++ Lambda/UsersEndpoint/deck/create.js
@@ -1,3 +1,7 @@
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-east-2" });
+const DynamoDB = new AWS.DynamoDB.DocumentClient();
+
 const { blankDeck } = require("../config/config.js");
 const auth = require("./utils/middleware.js");
 
@@ -10,7 +14,6 @@ async function create(body, token) {
    if (!username) return { statusCode: 401, body: { errors: [{ msg: "Unauthorized, token invalid" }] } };
 
    const { deckName } = body;
-
    const params = {
       TableName: "users",
       Key: { username },
