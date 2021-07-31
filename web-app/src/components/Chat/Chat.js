8ba9d6e6988485895c9f6e4e3f14b586a0c1e477
acpennington <penningtona@mail.usf.edu>
2021-07-31 14:33:06 -0400

    effect chat message

--- web-app/src/components/Chat/Chat.js
+++ web-app/src/components/Chat/Chat.js
@@ -1,6 +1,7 @@
 import React, { PureComponent, Fragment } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { bind, unbind } from "mousetrap";
 
 import Button from "components/CustomButtons/Button.js";
 import ButtonRow from "components/CustomButtons/ButtonRow.js";
@@ -11,12 +12,23 @@ import FriendlyScroll from "components/FriendlyScroll/FriendlyScroll.js";
 import { addMessage } from "stateStore/actions/game/chat.js";
 import { WebSocketContext } from "views/Game/WebSocketContext.js";
 
+import { FIELD_SPELL, HAND } from "utils/constants.js";
+
 import { withStyles } from "@material-ui/core/styles";
 import chatStyle from "assets/jss/material-kit-react/components/chatStyle.js";
 
-const cannedMessages = ["Yes", "No", "Ok", "Thinking", "Response?"];
+const EFFECT_MESSAGE = "Effect!";
+const cannedMessages = ["Ok", "No", "Thinking", "Response?", EFFECT_MESSAGE];
 
 class Chat extends PureComponent {
+   componentDidMount() {
+      bind("f", () => this.sendMessage(this.props.name, EFFECT_MESSAGE));
+   }
+
+   componentWillUnmount() {
+      unbind(["f"]);
+   }
+
    submitMessage = (event) => {
       if (event.key === "Enter") {
          this.sendMessage(this.props.name, event.target.value);
@@ -25,8 +37,15 @@ class Chat extends PureComponent {
    };
 
    sendMessage = (author, content) => {
-      const { chat, addMessage } = this.props;
+      const { chat, addMessage, heroSelected, heroField } = this.props;
       content = content.trim();
+
+      if (content === EFFECT_MESSAGE && heroSelected) {
+         const { row, zone } = heroSelected;
+         const card = row === FIELD_SPELL ? heroField[FIELD_SPELL] : heroField[row][zone];
+         if (row !== HAND && !card.facedown) content = "Activate the effect of " + card.name + ".";
+      }
+
       if (content) {
          const lastMessage = chat[chat.length - 1];
          if (!(lastMessage && lastMessage.author === author && lastMessage.content === content)) addMessage(author, content, this.context);
@@ -68,8 +87,8 @@ class Chat extends PureComponent {
    }
 }
 
-function mapStateToProps(state) {
-   return { chat: state.chat };
+function mapStateToProps(state, ownProps) {
+   return { chat: state.chat, heroSelected: state.selectedCard[ownProps.name], heroField: state.field[ownProps.name] };
 }
 
 Chat.propTypes = {
