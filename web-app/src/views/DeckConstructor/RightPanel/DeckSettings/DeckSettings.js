c9784a9fab6a59ece6acc04cd2ebce51839e21ea
acpennington <penningtona@mail.usf.edu>
2021-08-06 20:52:29 -0400

    add fusions button

--- web-app/src/views/DeckConstructor/RightPanel/DeckSettings/DeckSettings.js
+++ web-app/src/views/DeckConstructor/RightPanel/DeckSettings/DeckSettings.js
@@ -1,10 +1,13 @@
 import React, { PureComponent } from "react";
 
 import FriendlyScroll from "components/FriendlyScroll/FriendlyScroll.js";
+import Button from "components/CustomButtons/Button.js";
 import DeckSelector from "./DeckSelector.js";
 import ResizeCards from "./ResizeCards.js";
 import BackButton from "components/CustomButtons/BackButton.js";
 import Switches from "./Switches.js";
+
+import { IoMdHelpCircle } from "react-icons/io";
 
 import { withStyles } from "@material-ui/core/styles";
 import styles from "assets/jss/material-kit-react/views/deckConstructorSections/rightPanel.js";
@@ -17,6 +20,11 @@ class DeckSettings extends PureComponent {
          <div className={classes.container}>
             <FriendlyScroll id="deckSettings" flexDirection="column">
                <BackButton href="wall" size="lg" />
+               <Button color="info" size="lg" round href="/fusions" target="_blank">
+                  <IoMdHelpCircle />
+                  Fusions
+                  <IoMdHelpCircle style={{ marginLeft: "5px" }} />
+               </Button>
                <ResizeCards />
                <Switches />
                <DeckSelector />
