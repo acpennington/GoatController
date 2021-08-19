29bbc279c74693901fc05ffb1d5e7dfedcd23dee
Kirk Scheibelhut <kjs@scheibo.com>
2021-08-19 12:27:32 -0700

    Add periods

--- web-app/src/databases/cardDB/spells/ritualSpells.js
+++ web-app/src/databases/cardDB/spells/ritualSpells.js
@@ -5,37 +5,37 @@ const ritualSpells = {
       id: "41426869",
       cardType: SPELL,
       levelOrSubtype: "Ritual",
-      text: 'This card is used to Ritual Summon "Relinquished". You must also Tribute a monster from your hand or field whose Level is 1 or more'
+      text: 'This card is used to Ritual Summon "Relinquished". You must also Tribute a monster from your hand or field whose Level is 1 or more.'
    },
    "Black Luster Ritual": {
       id: "55761792",
       cardType: SPELL,
       levelOrSubtype: "Ritual",
-      text: 'This card is used to Ritual Summon "Black Luster Soldier". You must also Tribute monsters from your hand or field whose total Levels equal 8 or more'
+      text: 'This card is used to Ritual Summon "Black Luster Soldier". You must also Tribute monsters from your hand or field whose total Levels equal 8 or more.'
    },
    "Commencement Dance": {
       id: "43417563",
       cardType: SPELL,
       levelOrSubtype: "Ritual",
-      text: 'This card is used to Ritual Summon "Performance of Sword". You must also Tribute monsters from your hand or field whose totale Levels equal 6 or more'
+      text: 'This card is used to Ritual Summon "Performance of Sword". You must also Tribute monsters from your hand or field whose totale Levels equal 6 or more.'
    },
    "Contract with the Abyss": {
       id: "69035382",
       cardType: SPELL,
       levelOrSubtype: "Ritual",
-      text: "This card can be used to Ritual Summon any DARK Ritual Monster. You must also Tribute monsters from your hand or field whose total Levels exactly equal the Level of the Ritual Monster you Ritual Summon"
+      text: "This card can be used to Ritual Summon any DARK Ritual Monster. You must also Tribute monsters from your hand or field whose total Levels exactly equal the Level of the Ritual Monster you Ritual Summon."
    },
    "Contract with the Dark Master": {
       id: "96420087",
       cardType: SPELL,
       levelOrSubtype: "Ritual",
-      text: 'This card is used to Ritual Summon "Dark Master - Zorc". You must also Tribute monsters from your hand or field whose total Levels equal 8 or more'
+      text: 'This card is used to Ritual Summon "Dark Master - Zorc". You must also Tribute monsters from your hand or field whose total Levels equal 8 or more.'
    },
    "Curse of the Masked Beast": {
       id: "94377247",
       cardType: SPELL,
       levelOrSubtype: "Ritual",
-      text: 'This card is used to Ritual Summon "The Masked Beast". You must also Tribute monsters from your hand or field whose total Levels equal 8 or more'
+      text: 'This card is used to Ritual Summon "The Masked Beast". You must also Tribute monsters from your hand or field whose total Levels equal 8 or more.'
    },
    "Doriado's Blessing": {
       id: "23965037",
@@ -59,7 +59,7 @@ const ritualSpells = {
       id: "80811661",
       cardType: SPELL,
       levelOrSubtype: "Ritual",
-      text: 'This card is used to Ritual Summon "Hungry Burger". You must also Tribute monsters whose total Level Stars equal 6 or more from the field or your hand'
+      text: 'This card is used to Ritual Summon "Hungry Burger". You must also Tribute monsters whose total Level Stars equal 6 or more from the field or your hand.'
    },
    "Incandescent Ordeal": {
       id: "33031674",
@@ -71,13 +71,13 @@ const ritualSpells = {
       id: "43694075",
       cardType: SPELL,
       levelOrSubtype: "Ritual",
-      text: 'This card is used to Ritual Summon "Skull Guardian". You must also offer monsters whose total Level Stars equal 7 or more as a Tribute from the field or your hand'
+      text: 'This card is used to Ritual Summon "Skull Guardian". You must also offer monsters whose total Level Stars equal 7 or more as a Tribute from the field or your hand.'
    },
    "Revival of Dokurorider": {
       id: "31066283",
       cardType: SPELL,
       levelOrSubtype: "Ritual",
-      text: 'This card is used to Ritual Summon "Dokurorider". You must also offer monsters whose total Level Stars equal 6 or more as a Tribute from the field or your hand'
+      text: 'This card is used to Ritual Summon "Dokurorider". You must also offer monsters whose total Level Stars equal 6 or more as a Tribute from the field or your hand.'
    },
    "Shinato's Ark": {
       id: "60365591",
@@ -89,13 +89,13 @@ const ritualSpells = {
       id: "76806714",
       cardType: SPELL,
       levelOrSubtype: "Ritual",
-      text: 'This card is used to Ritual Summon "Crab Turtle". You must also Tribute monsters from your hand or field whose total Levels equal 8 or more'
+      text: 'This card is used to Ritual Summon "Crab Turtle". You must also Tribute monsters from your hand or field whose total Levels equal 8 or more.'
    },
    "White Dragon Ritual": {
       id: "09786492",
       cardType: SPELL,
       levelOrSubtype: "Ritual",
-      text: 'This card is used to Ritual Summon "Paladin of White Dragon". You must also Tribute monsters from your hand or field whose total Levels equal 4 or more'
+      text: 'This card is used to Ritual Summon "Paladin of White Dragon". You must also Tribute monsters from your hand or field whose total Levels equal 4 or more.'
    },
 };
 
