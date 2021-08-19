56dded980dd04a2845304e000ead53e12b66ec8c
Kirk Scheibelhut <kjs@scheibo.com>
2021-08-18 20:13:58 -0700

    Update MILL_UNTIL script to handle failure

--- Lambda/GameSocket/Mill.js
+++ Lambda/GameSocket/Mill.js
@@ -5,12 +5,15 @@ const display = require("./utils/display");
 // @desc Sends a payload to others about a card being milled
 // @access Private
 // @db 1 read, 0 writes
-async function mill(id, username, deck, params, connectionId, api) {
+async function mill(id, username, deck, params, fail, connectionId, api) {
    // This is a really hacky way to check for Merchant vs Reasoning/Gate
    const cardType = params.cardType && params.cardType.operator === "OR" ? "Spell or Trap" : "Normal Summon Monster";
 
-   const message = { author: "Server", content: `${username} milled until a ${cardType} card.` };
-   const action = { action: "MILL_UNTIL", data: { player: username, deck, params } };
+   const content =  fail
+      ? `${username} was unable to mill until a ${cardType} card as there are no legal targets remaining.`
+      : `${username} milled until a ${cardType} card.`;
+   const message = { author: "Server", content };
+   const action = { action: "MILL_UNTIL", data: { player: username, deck, params, fail } };
 
    await actionAndMessage(id, action, message, connectionId, api);
    return { statusCode: 200, body: "Card milled" };
