26be437d7c81ff07d73c9448bb877e10d1786ad4
Kirk Scheibelhut <kjs@scheibo.com>
2021-08-21 13:02:12 -0700

    Add build script

---
+++ build.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+for (const destination of ['Lambda/GameSocket', 'Lambda/LeagueSocket']) {
+  const dst = path.resolve(__dirname, destination, 'shared');
+  if (!fs.existsSync(dst)) fs.mkdirSync(dst);
+  for (const file of fs.readdirSync(path.resolve(__dirname, 'shared'))) {
+    if (!file.endsWith('.js') && !file.endsWith('.json')) continue;
+    fs.copyFileSync(path.resolve(__dirname, 'shared', file), path.resolve(dst, file));
+  }
+}
