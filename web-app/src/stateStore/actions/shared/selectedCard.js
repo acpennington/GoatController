dbe0e6767eb1ec71fb8cf9a34b3340ca98abf240
Allen Pennington <acpenni@evoforge.com>
2021-08-27 13:54:32 -0400

    additional checks on prepop

--- web-app/src/stateStore/actions/shared/selectedCard.js
+++ web-app/src/stateStore/actions/shared/selectedCard.js
@@ -1,14 +1,14 @@
-import { NEW_SELECTION, CLEAR_SELECTION } from "shared/constants.js";
+import { NEW_SELECTION, CLEAR_SELECTION, HAND } from "shared/constants.js";
 import { prepopLP } from "./settings.js";
 import getCardDetails from "shared/getCardDetails.js";
 
 function newSelection(selectingPlayer, player, row, zone, name, facedown = false, socket = false) {
    return (dispatch) => {
       dispatch({ type: NEW_SELECTION, data: { selectingPlayer, socket, player, row, zone, name, facedown } });
-      const card = getCardDetails(name);
-      if (card.prepopLP) {
-         if (selectingPlayer === player) dispatch(prepopLP(card.prepopLP.hero));
-         else dispatch(prepopLP(card.prepopLP.villain));
+      const { prepopLP: cardPrepop } = getCardDetails(name);
+      if (cardPrepop && row !== HAND && !facedown) {
+         if (selectingPlayer === player) dispatch(prepopLP(cardPrepop.hero));
+         else dispatch(prepopLP(cardPrepop.villain));
       } else dispatch(prepopLP(null));
    };
 }
