8534c1c888913d7d4b647cb96c8fe8c28841dc83
acpennington <penningtona@mail.usf.edu>
2021-09-03 09:27:29 -0400

    fix token thanksgiving/feastevil bug

--- shared/getCardDetails.js
+++ shared/getCardDetails.js
@@ -13,6 +13,6 @@ const emptyCard = {
 module.exports = function getCardDetails(raw) {
    if (!raw) return false;
    const name = raw.split(SENTINEL)[0];
-   if (name.includes("Token")) return tokens[name] ? tokens[name] : emptyCard;
+   if (name !== "Token Thanksgiving" && name !== "Token Feastevil" && name.includes("Token")) return tokens[name] ? tokens[name] : emptyCard;
    return cards[name] ? cards[name] : emptyCard;
 };
